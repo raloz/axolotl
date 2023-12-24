83d09e4a1b9cf9d34cbd483035fa5959445f51c0
Octavio Quiroz <octavioquiroz30@gmail.com>
2023-12-24 14:43:25 -0600

    feat: se crea el pryecto de vite+react typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-center">
+        Axolotl Chrome Extension{" "}
+        <img
+          className="inline w-10 h-10"
+          src="./icons/axolotl128.png"
+          alt="axolotl logo"
+        />
+      </h1>
+    </>
+  );
+}
+
+export default App;
